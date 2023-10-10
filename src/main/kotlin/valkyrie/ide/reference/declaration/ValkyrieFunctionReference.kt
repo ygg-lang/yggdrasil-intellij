b09ac1a6e4d0b253485ab795af5641a20cd48a79
Aster <aster@vers.site>
2023-10-11 00:33:50 +0800

    Remove ast transform actions

--- src/main/kotlin/valkyrie/ide/reference/declaration/ValkyrieFunctionReference.kt
+++
@@ -1,9 +0,0 @@
-package valkyrie.ide.reference.declaration
-
-import yggdrasil.language.ast.YggdrasilFunctionStatement
-import yggdrasil.language.ast.YggdrasilIdentifierNode
-
-class ValkyrieFunctionReference(definition: YggdrasilFunctionStatement, element: YggdrasilIdentifierNode) :
-    ValkyrieReference(element, definition) {
-
-}
